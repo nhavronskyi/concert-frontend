63bdbbca0bc828e717ea1dbdc0127e1f6e1ffc40
Oksana Lahutina <oksankalahutina@gmail.com>
2023-06-04 14:34:38 +0300

    Added rich text editor (Draft.js)

---
+++ src/components/show-event-page/TextEditor.tsx
@@ -0,0 +1,107 @@
+import clsx from "clsx";
+
+type StyleButtonProps = {
+    onClick?: () => void;
+    onToggle?: (style: string) => void;
+    active?: boolean;
+    className?: string;
+    style?: string;
+    label: string;
+};
+
+const StyleButton = (props: StyleButtonProps) => {
+    const handleMouseDown = (e: React.MouseEvent<HTMLSpanElement>) => {
+        e.preventDefault();
+        props?.onToggle!(props.style || "");
+    };
+
+    const handleClick = (e: React.MouseEvent<HTMLSpanElement>) => {
+        e.preventDefault();
+
+        if (props?.onClick) {
+            props.onClick();
+        }
+    };
+
+    const className = clsx(
+        "toolbar-button",
+        props.active && "toolbar-button-active",
+        props.className
+    );
+
+    return (
+        <span
+            className={className}
+            onMouseDown={handleMouseDown}
+            onClick={handleClick}
+        >
+      {props.label}
+    </span>
+    );
+};
+
+const BLOCK_TYPES = [
+    { label: "H1", style: "header-one" },
+    { label: "H2", style: "header-two" },
+    { label: "H3", style: "header-three" },
+    { label: "H4", style: "header-four" },
+    { label: "H5", style: "header-five" },
+    { label: "H6", style: "header-six" },
+    { label: "Blockquote", style: "blockquote" },
+    { label: "Unordered list", style: "unordered-list-item" },
+    { label: "Ordered list", style: "ordered-list-item" },
+];
+
+const BlockStyleControls = (props: any) => {
+    const { editorState } = props;
+    const selection = editorState.getSelection();
+    const blockType = editorState
+        .getCurrentContent()
+        .getBlockForKey(selection.getStartKey())
+        .getType();
+
+    return (
+        <div className="toolbar-controls">
+            {BLOCK_TYPES.map((type) => (
+                <StyleButton
+                    key={type.label}
+                    active={type.style === blockType}
+                    label={type.label}
+                    onToggle={props.onToggle}
+                    style={type.style}
+                />
+            ))}
+        </div>
+    );
+};
+
+var INLINE_STYLES = [
+    { label: "Bold", style: "BOLD" },
+    { label: "Italic", style: "ITALIC" },
+    { label: "Underline", style: "UNDERLINE" },
+];
+
+const InlineStyleControls = (props: any) => {
+    const currentStyle = props.editorState.getCurrentInlineStyle();
+
+    return (
+        <div className="toolbar__controls">
+            {INLINE_STYLES.map((type) => (
+                <StyleButton
+                    key={type.label}
+                    active={currentStyle.has(type.style)}
+                    label={type.label}
+                    onToggle={props.onToggle}
+                    style={type.style}
+                />
+            ))}
+        </div>
+    );
+};
+
+export { BlockStyleControls };
+export { InlineStyleControls };
+
+export {BLOCK_TYPES};
+export {INLINE_STYLES};
+
